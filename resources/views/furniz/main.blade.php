05a55859568089d9d019fb1f5e6582c091f2cf14
T480SD\TuanDUY <tuanduy.truong@gmail.com>
2024-03-25 19:54:17 +0700

    - copy source html to public - organize folder view

---
+++ resources/views/furniz/main.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('furniz.elements.head')
+</head>
+
+<body class="bg-effect">
+
+    @include('furniz.elements.loader')
+    @include('furniz.elements.header')
+    @include('furniz.elements.mobile-menu')
+
+    @php
+        if ($_SERVER['REQUEST_URI'] == '/dat-hang-thanh-cong') {
+            include '../resources/views/furniz/blocks/breadcrumb-order.blade.php';
+        } elseif ($_SERVER['REQUEST_URI'] !== '/') {
+            include '../resources/views/furniz/blocks/breadcrumb.blade.php';
+        }
+    @endphp
+
+    @yield('content')
+
+    @php
+        if ($_SERVER['REQUEST_URI'] == '/') {
+            include '../resources/views/furniz/blocks/newsletter.blade.php';
+            include '../resources/views/furniz/blocks/cookie-bar-box.blade.php';
+        }
+    @endphp
+
+    @include('furniz.elements.footer')
+
+    @php
+        if (
+            $_SERVER['REQUEST_URI'] == '/' or
+            $_SERVER['REQUEST_URI'] == '/san-pham' or
+            $_SERVER['REQUEST_URI'] == '/danh-muc-san-pham'
+        ) {
+            include '../resources/views/furniz/blocks/quick-view-modal-box.blade.php';
+        }
+    @endphp
+
+    @include('furniz.blocks.location-modal')
+    @include('furniz.blocks.deal-modal-box')
+
+    @php
+        if ($_SERVER['REQUEST_URI'] == '/san-pham' or $_SERVER['REQUEST_URI'] == '/danh-muc-san-pham') {
+            include '../resources/views/furniz/blocks/add-to-cart.blade.php';
+        }
+    @endphp
+
+    @include('furniz.blocks.tap-to-top')
+
+    @php
+        if ($_SERVER['REQUEST_URI'] == '/san-pham') {
+            include '../resources/views/furniz/blocks/sticky-cart-box.blade.php';
+            include '../resources/views/furniz/blocks/review-modal.blade.php';
+        }
+    @endphp
+
+    @include('furniz.blocks.bg-overlay')
+
+    @php
+        if ($_SERVER['REQUEST_URI'] == '/bang-dieu-khien') {
+            include '../resources/views/furniz/blocks/dashboard/add-address.blade.php';
+            include '../resources/views/furniz/blocks/dashboard/edit-profile.blade.php';
+            include '../resources/views/furniz/blocks/dashboard/edit-card.blade.php';
+            include '../resources/views/furniz/blocks/dashboard/remove-profile.blade.php';
+        }
+    @endphp
+
+    @include('furniz.elements.script')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,54 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('furniz.pages.home.index');
+});
+
+Route::get('/danh-muc-san-pham', function () {
+    return view('furniz.pages.category-product.index');
+});
+
+Route::get('/san-pham', function () {
+    return view('furniz.pages.product.index');
+});
+
+Route::get('/danh-muc-bai-viet', function () {
+    return view('furniz.pages.category-article.index');
+});
+
+Route::get('/bai-viet', function () {
+    return view('furniz.pages.article.index');
+});
+
+Route::get('/gio-hang', function () {
+    return view('furniz.pages.cart.index');
+});
+
+Route::get('/thanh-toan', function () {
+    return view('furniz.pages.order.checkout');
+});
+
+Route::get('/dat-hang-thanh-cong', function () {
+    return view('furniz.pages.order.success');
+});
+
+Route::get('/tra-cuu-don-hang', function () {
+    return view('furniz.pages.order.tracking');
+});
+
+Route::get('/lien-he', function () {
+    return view('furniz.pages.contact-us.index');
+});
+
+Route::get('/bang-dieu-khien', function () {
+    return view('furniz.pages.dashboard.index');
+});
+
+Route::get('/gioi-thieu', function () {
+    return view('furniz.templates.about-us');
+});
+
+Route::get('/404', function () {
+    return view('furniz.templates.404');
 });
